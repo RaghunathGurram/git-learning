dd459c605b3d7e862642471e289e68ace6e54f7f
RaghunathGurram <46337575+RaghunathGurram@users.noreply.github.com>
2021-07-31 15:44:50 +0530

    NA

--- src/main/java/com/java/guides/angular/integration/angularintegration/CompleteAngularIntegrationApplication.java
+++ src/main/java/com/java/guides/angular/integration/angularintegration/CompleteAngularIntegrationApplication.java
@@ -8,7 +8,6 @@ public class CompleteAngularIntegrationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompleteAngularIntegrationApplication.class, args);
-
 	}
 
 }
